ae7f25c74d4d213cf89824db71587377591c0c8e
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-05-05 16:35:15 -0300

    Added an images array as a state variable that will store each image id and url. Added a function, called when the component mounts, that fecthes the API key, fetches the images and stores the images data in the images array. If the images array have data, the images are shown on the screen.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import APIKeyPath from "../firebase";
 
-let APIKey;
+function Search() {
+  const [images, setImages] = useState(undefined);
 
-(async function fetchAPIKey() {
+  useEffect(() => {
+    async function fetchImages() {
+      //Fetch API key
+      const doc = await APIKeyPath.get();
+      const APIKey = await doc.data().key;
+
+      //Fetch images urls
+      const fetchedData = await fetch(
+        `https://api.unsplash.com/search/photos?client_id=${APIKey}&query=cat`,
+        { mode: "cors" }
+      );
+      const fecthedDataJSON = await fetchedData.json();
+      console.log(fecthedDataJSON);
+      setImages(
+        fecthedDataJSON.results.map((img) => {
+          return { key: img.id, url: img.urls.regular };
+        })
+      );
+    }
+    fetchImages();
+  }, []);
+
+  return (
+    <div className="Search">
+      {images === undefined ? (
+        <h1>Undefined</h1>
+      ) : (
+        images.map((img) => {
+          return <img key={img.key} src={img.url} />;
+        })
+      )}
+    </div>
+  );
+}
+
+/*class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      APIKey: undefined,
+      imagesUrls: undefined,
+    };
+  }
+
+  componentDidMount() {
+
+
+
+
+    APIKeyPath.get().then((doc) => {
+      const APIKey = doc.data().key;
+      fetch(
+        `https://api.unsplash.com/search/photos?client_id=${APIKey}&query=cat`,
+        { mode: "cors" }
+      ).then((result) => {
+        console.log(result.json());
+      });
+      .then((data) => {
+          const urls = data.results.map((img) => {
+            return img.urls.regular;
+          });
+          this.setState({ imagesUrls: urls });
+
+          //this.setState({ APIKey: doc.data().key });
+        });
+    });
+  }
+
+  render() {
+    return <div className="Search"></div>;
+  }
+}*/
+
+/*{this.state.APIKey === undefined ? (
+          <h1>Undefined</h1>
+        ) : (
+          <h1>{this.state.APIKey}</h1>
+        )}*/
+
+//const [APIKey, setAPIKey] = useState(undefined);
+//const [results, setResults] = useState([]);
+//let results;
+//let APIKey;
+
+/*(async function fetchAPIKey() {
   const data = await APIKeyPath.get();
   APIKey = data.data().key;
+  //setAPIKey(data.data().key);
   console.log(APIKey);
+
+  const fetchImages = await fetch(
+    `https://api.unsplash.com/search/photos?client_id=${APIKey}&query=cat`,
+    { mode: "cors" }
+  );
+  const imagesDataJSON = await fetchImages.json();
+  storeResults(imagesDataJSON);
+  console.log(imagesDataJSON);
 })();
 
-function Search() {
-  return <div className="Search"></div>;
+function storeResults(json) {
+  const newResults = json.results.map((result) => {
+    return result.urls.regular;
+  });
+  results = newResults;
+  //setResults(newResults);
 }
+
+function Search() {
+  useEffect(() => {
+    // Do something
+  }, []);
+
+  return <div className="Search"></div>;
+}*/
 
 export default Search;
