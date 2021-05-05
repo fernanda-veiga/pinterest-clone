b72a9efae98f2eb01a396bc8f8773c496e899f8b
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-05-05 18:09:27 -0300

    Added a Loader component to be shown on the Search screen while the API is fetching the images data.

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        <div className="loader-circle"></div>
+        <div className="loader-circle"></div>
+        <div className="loader-circle"></div>
+        <div className="loader-circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import APIKeyPath from "../firebase";
+import Loader from "./Loader";
 
 function Search() {
   const [images, setImages] = useState(undefined);
@@ -29,7 +30,7 @@ function Search() {
   return (
     <div className="Search">
       {images === undefined ? (
-        <h1>Undefined</h1>
+        <Loader />
       ) : (
         images.map((img) => {
           return <img key={img.key} src={img.url} />;
@@ -38,85 +39,5 @@ function Search() {
     </div>
   );
 }
-
-/*class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      APIKey: undefined,
-      imagesUrls: undefined,
-    };
-  }
-
-  componentDidMount() {
-
-
-
-
-    APIKeyPath.get().then((doc) => {
-      const APIKey = doc.data().key;
-      fetch(
-        `https://api.unsplash.com/search/photos?client_id=${APIKey}&query=cat`,
-        { mode: "cors" }
-      ).then((result) => {
-        console.log(result.json());
-      });
-      .then((data) => {
-          const urls = data.results.map((img) => {
-            return img.urls.regular;
-          });
-          this.setState({ imagesUrls: urls });
-
-          //this.setState({ APIKey: doc.data().key });
-        });
-    });
-  }
-
-  render() {
-    return <div className="Search"></div>;
-  }
-}*/
-
-/*{this.state.APIKey === undefined ? (
-          <h1>Undefined</h1>
-        ) : (
-          <h1>{this.state.APIKey}</h1>
-        )}*/
-
-//const [APIKey, setAPIKey] = useState(undefined);
-//const [results, setResults] = useState([]);
-//let results;
-//let APIKey;
-
-/*(async function fetchAPIKey() {
-  const data = await APIKeyPath.get();
-  APIKey = data.data().key;
-  //setAPIKey(data.data().key);
-  console.log(APIKey);
-
-  const fetchImages = await fetch(
-    `https://api.unsplash.com/search/photos?client_id=${APIKey}&query=cat`,
-    { mode: "cors" }
-  );
-  const imagesDataJSON = await fetchImages.json();
-  storeResults(imagesDataJSON);
-  console.log(imagesDataJSON);
-})();
-
-function storeResults(json) {
-  const newResults = json.results.map((result) => {
-    return result.urls.regular;
-  });
-  results = newResults;
-  //setResults(newResults);
-}
-
-function Search() {
-  useEffect(() => {
-    // Do something
-  }, []);
-
-  return <div className="Search"></div>;
-}*/
 
 export default Search;
