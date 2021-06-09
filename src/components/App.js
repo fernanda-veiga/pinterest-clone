d4ce2f8b1664953e58363eb21237b03aa2cfac8e
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-09 11:23:29 -0300

    Added a Route to a path that is equal to /pin/<id>, such that id is a string of 11 digits and that contains only letters, numbers and a hash.

--- src/components/App.js
+++ src/components/App.js
@@ -24,27 +24,21 @@ function App() {
     let windowRelativeBottom = document.documentElement.getBoundingClientRect()
       .bottom;
     let windowHeight = document.documentElement.clientHeight;
-    console.log("Relative Bottom: ", windowRelativeBottom);
-    console.log("Window Height: ", windowHeight);
 
-    if (windowRelativeBottom - 1 < windowHeight) {
-      console.log("Reached the bottom");
-      console.log("Current page when reach bottom: ", currentPage);
+    if (windowRelativeBottom < windowHeight + 1) {
       setCurrentPage(currentPage + 1);
       fetchImagesOnScroll(searchTerm, currentPage);
     }
   }
 
   async function fetchImagesOnScroll(term, page) {
-    console.log("Current page for fetch on scroll: ", currentPage);
-
     //Fetch API key
     const doc = await APIKeyPath.get();
     const APIKey = await doc.data().key;
 
     //Fetch images urls
     const fetchedData = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=${page}&per_page=10&query=${term}`,
+      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=${page}&per_page=30&query=${term}`,
       { mode: "cors" }
     );
     const fecthedDataJSON = await fetchedData.json();
@@ -67,7 +61,7 @@ function App() {
   }
 
   async function fetchImagesOnSearch(term) {
-    console.log("Current page for fetch on search: ", currentPage);
+    //console.log("Current page for fetch on search: ", currentPage);
     setLoading(true);
     setError(false);
 
@@ -77,7 +71,7 @@ function App() {
 
     //Fetch images urls
     const fetchedData = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=1&per_page=10&query=${term}`,
+      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=1&per_page=30&query=${term}`,
       { mode: "cors" }
     );
     const fecthedDataJSON = await fetchedData.json();
@@ -88,14 +82,100 @@ function App() {
     } else {
       setImages(
         fecthedDataJSON.results.map((img) => {
-          return { key: img.id, url: img.urls.regular };
+          return {
+            key: img.id,
+            url: img.urls.regular,
+            alt: img.alt_description,
+          };
         })
       );
     }
 
     setLoading(false);
-    setCurrentPage(2);
+    setCurrentPage(3);
   }
+
+  /*function handleScroll() {
+    let windowRelativeBottom = document.documentElement.getBoundingClientRect()
+      .bottom;
+    let windowHeight = document.documentElement.clientHeight;
+    //console.log("Relative Bottom: ", windowRelativeBottom);
+    //console.log("Window Height: ", windowHeight);
+
+    if (windowRelativeBottom < windowHeight + 1) {
+      //console.log("Reached the bottom");
+      //console.log("Current page when reach bottom: ", currentPage);
+      setCurrentPage(currentPage + 1);
+      fetchImagesOnScroll(searchTerm, currentPage);
+    }
+  }
+
+  async function fetchImagesOnScroll(term, page) {
+    //console.log("Current page for fetch on scroll: ", currentPage);
+
+    //Fetch API key
+    const doc = await APIKeyPath.get();
+    const APIKey = await doc.data().key;
+
+    //Fetch images urls
+    const fetchedData = await fetch(
+      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=${page}&per_page=30&query=${term}`,
+      { mode: "cors" }
+    );
+    const fecthedDataJSON = await fetchedData.json();
+    console.log("Data for fetch on scroll: ", fecthedDataJSON);
+    const newImages = fecthedDataJSON.results.map((img) => {
+      return { key: img.id, url: img.urls.regular };
+    });
+
+    const imageContainer = document.querySelector(".Search-image-container");
+    for (let i = 0; i < newImages.length; i++) {
+      const img = document.createElement("img");
+      img.setAttribute("key", newImages[i].key);
+      img.setAttribute("src", newImages[i].url);
+      img.setAttribute("alt", "");
+      img.classList.add("Search-image");
+      imageContainer.appendChild(img);
+    }
+
+    setCurrentPage(currentPage + 1);
+  }
+
+  async function fetchImagesOnSearch(term) {
+    //console.log("Current page for fetch on search: ", currentPage);
+    setLoading(true);
+    setError(false);
+
+    //Fetch API key
+    const doc = await APIKeyPath.get();
+    const APIKey = await doc.data().key;
+
+    //Fetch images urls
+    const fetchedData = await fetch(
+      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=1&per_page=30&query=${term}`,
+      { mode: "cors" }
+    );
+    const fecthedDataJSON = await fetchedData.json();
+    console.log("Data for fetch on search: ", fecthedDataJSON);
+
+    if (fecthedDataJSON.total === 0) {
+      setError(true);
+    } else {
+      setImages(
+        fecthedDataJSON.results.map((img) => {
+          return {
+            key: img.id,
+            url: img.urls.regular,
+            alt: img.alt_description,
+          };
+        })
+      );
+    }
+
+    setLoading(false);
+    setCurrentPage(3);
+  }*/
+  const IMG_PATH_REGEX = "([a-zA-Z0-9-]{11})";
 
   return (
     <HashRouter basename="/">
@@ -119,11 +199,16 @@ function App() {
               />
             )}
           />
+          <Route path={"/pin/:id" + IMG_PATH_REGEX} component={Test} />
           {/*<Route exact path="/user" component={User} />*/}
         </Switch>
       </div>
     </HashRouter>
   );
+}
+
+function Test() {
+  return <div>Hi</div>;
 }
 
 export default App;
