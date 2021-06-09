e8220a0ffc9c1c6f1e528772b2173b5803d8ce8f
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-09 18:15:19 -0300

    Commented the code for the image fetch on scroll. For the fetch images on search function, added more parameters to be saved in the image object.

--- src/components/App.js
+++ src/components/App.js
@@ -13,107 +13,26 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
+  /*useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  });
-
-  function handleScroll() {
-    let windowRelativeBottom = document.documentElement.getBoundingClientRect()
-      .bottom;
-    let windowHeight = document.documentElement.clientHeight;
-
-    if (windowRelativeBottom < windowHeight + 1) {
-      setCurrentPage(currentPage + 1);
-      fetchImagesOnScroll(searchTerm, currentPage);
-    }
-  }
-
-  async function fetchImagesOnScroll(term, page) {
-    //Fetch API key
-    const doc = await APIKeyPath.get();
-    const APIKey = await doc.data().key;
-
-    //Fetch images urls
-    const fetchedData = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=${page}&per_page=30&query=${term}`,
-      { mode: "cors" }
-    );
-    const fecthedDataJSON = await fetchedData.json();
-    console.log("Data for fetch on scroll: ", fecthedDataJSON);
-    const newImages = fecthedDataJSON.results.map((img) => {
-      return { key: img.id, url: img.urls.regular };
-    });
-
-    const imageContainer = document.querySelector(".Search-image-container");
-    for (let i = 0; i < newImages.length; i++) {
-      const img = document.createElement("img");
-      img.setAttribute("key", newImages[i].key);
-      img.setAttribute("src", newImages[i].url);
-      img.setAttribute("alt", "");
-      img.classList.add("Search-image");
-      imageContainer.appendChild(img);
-    }
-
-    setCurrentPage(currentPage + 1);
-  }
-
-  async function fetchImagesOnSearch(term) {
-    //console.log("Current page for fetch on search: ", currentPage);
-    setLoading(true);
-    setError(false);
-
-    //Fetch API key
-    const doc = await APIKeyPath.get();
-    const APIKey = await doc.data().key;
-
-    //Fetch images urls
-    const fetchedData = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${APIKey}&page=1&per_page=30&query=${term}`,
-      { mode: "cors" }
-    );
-    const fecthedDataJSON = await fetchedData.json();
-    console.log("Data for fetch on search: ", fecthedDataJSON);
-
-    if (fecthedDataJSON.total === 0) {
-      setError(true);
-    } else {
-      setImages(
-        fecthedDataJSON.results.map((img) => {
-          return {
-            key: img.id,
-            url: img.urls.regular,
-            alt: img.alt_description,
-          };
-        })
-      );
-    }
-
-    setLoading(false);
-    setCurrentPage(3);
-  }
+  });*/
 
   /*function handleScroll() {
     let windowRelativeBottom = document.documentElement.getBoundingClientRect()
       .bottom;
     let windowHeight = document.documentElement.clientHeight;
-    //console.log("Relative Bottom: ", windowRelativeBottom);
-    //console.log("Window Height: ", windowHeight);
 
     if (windowRelativeBottom < windowHeight + 1) {
-      //console.log("Reached the bottom");
-      //console.log("Current page when reach bottom: ", currentPage);
       setCurrentPage(currentPage + 1);
       fetchImagesOnScroll(searchTerm, currentPage);
     }
-  }
+  }*/
 
-  async function fetchImagesOnScroll(term, page) {
-    //console.log("Current page for fetch on scroll: ", currentPage);
-
+  /*async function fetchImagesOnScroll(term, page) {
     //Fetch API key
     const doc = await APIKeyPath.get();
     const APIKey = await doc.data().key;
@@ -126,13 +45,13 @@ function App() {
     const fecthedDataJSON = await fetchedData.json();
     console.log("Data for fetch on scroll: ", fecthedDataJSON);
     const newImages = fecthedDataJSON.results.map((img) => {
-      return { key: img.id, url: img.urls.regular };
+      return { id: img.id, url: img.urls.regular };
     });
 
     const imageContainer = document.querySelector(".Search-image-container");
     for (let i = 0; i < newImages.length; i++) {
       const img = document.createElement("img");
-      img.setAttribute("key", newImages[i].key);
+      img.setAttribute("key", newImages[i].id);
       img.setAttribute("src", newImages[i].url);
       img.setAttribute("alt", "");
       img.classList.add("Search-image");
@@ -140,7 +59,7 @@ function App() {
     }
 
     setCurrentPage(currentPage + 1);
-  }
+  }*/
 
   async function fetchImagesOnSearch(term) {
     //console.log("Current page for fetch on search: ", currentPage);
@@ -165,9 +84,14 @@ function App() {
       setImages(
         fecthedDataJSON.results.map((img) => {
           return {
-            key: img.id,
+            id: img.id,
+            link: img.links.html,
             url: img.urls.regular,
             alt: img.alt_description,
+            creator: {
+              profilePicture: img.user.profile_image.large,
+              name: img.user.name,
+            },
           };
         })
       );
@@ -175,7 +99,8 @@ function App() {
 
     setLoading(false);
     setCurrentPage(3);
-  }*/
+  }
+
   const IMG_PATH_REGEX = "([a-zA-Z0-9-]{11})";
 
   return (
