a56e10e1f128e3cd95421756fd705a97e435db2b
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-06-09 18:17:10 -0300

    In the Pin component, added the necessary elements to show the image information. Added links to take the user to the original image website. Added a button that returns the user to the previous page. Added styling to these elements.

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,17 +1,68 @@
 import React from "react";
+import arrowLeft from "../assets/arrow-left.svg";
+
+function PinLeft(props) {
+  const clickedImage = props.clickedImage;
+  return (
+    <div className="Pin-container-left">
+      <a className="Pin-image-url" href={clickedImage.url} target="blank_">
+        <span></span>
+        <img
+          className="Pin-image"
+          src={clickedImage.url}
+          alt={clickedImage.alt === null ? "" : clickedImage.alt}
+        />
+      </a>
+    </div>
+  );
+}
+
+function PinRight(props) {
+  const clickedImage = props.clickedImage;
+  return (
+    <div className="Pin-container-right">
+      <span className="Pin-container-right-top">
+        <a className="Pin-link" href={clickedImage.link} target="blank_">
+          unsplash.com
+        </a>
+        <button className="Pin-save-button">Save</button>
+      </span>
+
+      <h1 className="Pin-title">
+        {clickedImage.alt[0].toUpperCase() + clickedImage.alt.slice(1)}
+      </h1>
+
+      <span className="Pin-creator-container">
+        <img
+          className="Pin-creator-image"
+          src={clickedImage.creator.profilePicture}
+        />
+        <p className="Pin-creator-name">{clickedImage.creator.name}</p>
+      </span>
+    </div>
+  );
+}
 
 function Pin(props) {
   const images = props.images;
   const imageId = props.id;
-  const imageIndex = images.map((img) => img.key).indexOf(imageId);
+  const imageIndex = images.map((img) => img.id).indexOf(imageId);
   const clickedImage = images[imageIndex];
 
+  function returnToPreviousPage() {
+    window.history.back();
+  }
+
   return (
-    <div>
-      <img
-        src={clickedImage.url}
-        alt={clickedImage.alt === null ? "" : clickedImage.alt}
-      />
+    <div className="Pin">
+      <button className="back-button" onClick={returnToPreviousPage}>
+        <img className="back-icon" src={arrowLeft} alt="" />
+      </button>
+
+      <div className="Pin-container">
+        <PinLeft clickedImage={clickedImage} />
+        <PinRight clickedImage={clickedImage} />
+      </div>
     </div>
   );
 }
